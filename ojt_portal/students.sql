0bc4c08e65d9b3f5da1bb92b55d8e43b2c0305ba
ANDREIdotcom <riofrioalbertandrei_bscs@plmun.edu.ph>
2025-09-17 17:46:38 +0800

    Update students.sql

--- ojt_portal/students.sql
+++ ojt_portal/students.sql
@@ -3,50 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 06, 2025 at 12:23 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `school_portal`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `students`
---
-
-CREATE TABLE `students` (
-  `student_id` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `course` varchar(50) DEFAULT NULL,
-  `year_level` int(11) DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `students`
---
-
-INSERT INTO `students` (`stude-- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Sep 13, 2025 at 03:58 PM
+-- Generation Time: Sep 17, 2025 at 11:41 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -73,7 +30,9 @@ SET time_zone = "+00:00";
 CREATE TABLE `students` (
   `student_id` int(11) NOT NULL,
   `student_number` bigint(20) NOT NULL,
-  `name` varchar(100) NOT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `middle_name` varchar(50) DEFAULT NULL,
   `email` varchar(100) NOT NULL,
   `address` varchar(255) NOT NULL,
   `course` varchar(100) NOT NULL,
@@ -85,8 +44,9 @@ CREATE TABLE `students` (
 -- Dumping data for table `students`
 --
 
-INSERT INTO `students` (`student_id`, `student_number`, `name`, `email`, `address`, `course`, `password`, `created_at`) VALUES
-(1, 22144102, '123123', 'test@test.com', '123123123', '123123', '2003', '2025-09-13 13:52:29');
+INSERT INTO `students` (`student_id`, `student_number`, `last_name`, `first_name`, `middle_name`, `email`, `address`, `course`, `password`, `created_at`) VALUES
+(1, 22144102, 'Riofrio', 'Albert Andrei', 'Martinez', 'andrei@riofrio.com', 'gumamela st', 'BSCS', '2003', '2025-09-13 13:52:29'),
+(2, 22144103, 'Gutierrez', 'Mariella', '', 'mariella@gutierrez.com', 'Sample Street', 'BSIT', '1234', '2025-09-17 09:28:27');
 
 --
 -- Indexes for dumped tables
@@ -98,35 +58,6 @@ INSERT INTO `students` (`student_id`, `student_number`, `name`, `email`, `addres
 ALTER TABLE `students`
   ADD PRIMARY KEY (`student_id`),
   ADD UNIQUE KEY `student_number` (`student_number`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `students`
---
-ALTER TABLE `students`
-  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-nt_id`, `name`, `email`, `password`, `course`, `year_level`, `created_at`) VALUES
-(1, 'Test Student', 'student@test.com', '12345', 'BSCS', 3, '2025-09-06 09:36:42'),
-(2, 'Andrei', 'andrei@test.com', '12345', ' BSCS', 3, '2025-09-06 09:42:02');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `students`
---
-ALTER TABLE `students`
-  ADD PRIMARY KEY (`student_id`),
-  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
